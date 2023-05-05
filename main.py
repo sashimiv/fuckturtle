0ad399eb4d5976436e871735c9613f0d4964c57a
Vladimir Zolototrubov <74048469+sashimiv@users.noreply.github.com>
2023-05-05 12:14:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import *
+speed(100)
+pensize(2)
+color("navy")
+size = 10
+for i in range(70):
+    circle(size)
+    size += 1
+    
+left(906)
+size = 10
+for i in range(70):
+    circle(size)
+    size += 1
+    
+left(900)
+size = 10
+for i in range(70):
+    circle(size)
+    size += 1
+    
+left(100)
+size = 10
+for i in range(70):
+    circle(size)
+    size += 1
+hideturtle()
+exitonclick()
